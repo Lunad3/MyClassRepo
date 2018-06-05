916d1247e3d436e993431df637f510c82739ae28
Lunad3 <123.daniel.luna@gmail.com>
2018-06-05 14:32:08 -0700

    zombie game

--- 08-nodejs/01-Activities/29-ZombieChallenge/Unsolved/zombiegame.js
+++ 08-nodejs/01-Activities/29-ZombieChallenge/Unsolved/zombiegame.js
@@ -14,3 +14,40 @@
 // Major Warning: inquirer's prompt function is "asynchronous", which means that the majority of your game logic will need to be inside the .then() function for your prompt. 
 
 // ===========================================================================================================
+var iqrr = require("inquirer");
+
+var player = {
+    health:100,
+    guess: 0,
+    makeGuess:function(){
+        iqrr.prompt([{
+            type:"list",
+            message:"Try to stay alive! Guess a number between [1-5]",
+            choices:[1,2,3,4,5],
+            name: "guess"
+        }]).then(function(round){
+            player.guess = round.guess;
+        });
+    }
+};
+
+var zombie = {
+    health: 50,
+    guess:function(){
+        return Math.floor(Math.random() * (6 - 1) ) + 1
+    }
+};
+
+while (player.health > 0 && zombie.health > 0){
+    player.makeGuess;
+    if (player.guess == zombie.guess){
+        zombie.health -= player.guess;
+    }
+    else {
+        var damage = zombie.guess;
+        player.health -= damage;
+        console.log("Zombie attacked you with " + damage.toString() + " damage, you have " + player.health.toString() + " Health left, zombie has " + zombie.health.toString() + "health left");
+        
+    }
+
+}
