fb5d21095b3a362671ac27a9e6d64d9e3b5dfb3a
Taylor Blanche <john.t.blanche@gmail.com>
2018-06-08 10:43:25 -0700

    push up changes

--- 09-js-constructors/01-Activities/15-Callbacks/Solved/callbacks.js
+++ 09-js-constructors/01-Activities/15-Callbacks/Solved/callbacks.js
@@ -6,17 +6,36 @@ function logBefore(func, message) {
   func();
 }
 
+logBefore(function () {
+  console.log('this is a function that is a callback.')
+}, 'this is just a parameter to the original function');
+
 function runIf(func, bool) {
   if (bool) {
     func();
   }
 }
 
+runIf(function () {
+  console.log('a');
+}, false);
+
+runIf(function () {
+  console.log('b');
+}, true);
+
 function wrap(func, value) {
   return function() {
     return func(value);
   };
 }
+
+var resultFunc = wrap(function (a) {
+  console.log('this is inside my wrap');
+  return a + 5;
+}, 7);
+console.log('this is outside my wrap');
+resultFunc();
 
 // Callbacks appear in familiar places, like fs.writeFile.
 fs.writeFile("log.txt", "Log message!", function(err) {
